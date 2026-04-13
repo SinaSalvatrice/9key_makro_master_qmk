28b1da90abb80cd77698dab65f39a0e88c204375
SinaSalvatrice <sinagessner.wuppertal@gmail.com>
2026-04-14 01:37:53 +0200

    Revert "Update keymap.c"
    This reverts commit ca18f6b11522e392fb1f6462baa3125f87f5c791.

--- keymaps/reworked/keymap.c
+++ keymaps/reworked/keymap.c
@@ -821,7 +821,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_DEV] = LAYOUT(
-        MO_SELECT,           MS_UP,              KC_LSFT,
+        MO_SELECT,         MS_UP,              KC_LSFT,
         MS_LEFT,             MS_BTN1,            MS_RGHT,
         KC_LALT,             MS_DOWN,            MS_BTN2
     ),
@@ -1060,7 +1060,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             break;
 
         case _RGB:
-            tap_code16(clockwise ? BL_UP : BL_DOWN);
+            tap_code16(clockwise ? UG_VALU : UG_VALD);
             break;
 
         case _DEV:
